60d75b5ba5116bd31e30f9e93fc09820410c332f
Brandon Marrero <brandonmar97@gmail.com>
2020-03-11 02:28:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,10 +6,10 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-array.collect do |word| 
-word.capitalize + "!"
-#   end 
-# end 
+  array.collect do |word| 
+    word.capitalize + "!"
+  end 
+end 
 
 def long_planeteer_calls# code an argument here
   # Your code here
