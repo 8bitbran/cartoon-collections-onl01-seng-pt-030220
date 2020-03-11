4f6775aa7a07e67c6459afd9cfa3f37bfccfb911
Brandon Marrero <brandonmar97@gmail.com>
2020-03-11 02:21:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect(&:capitalize)
-    
+  array.collect do |call|
+    array[call] = array[call].capitalize
   end   
 end
 
